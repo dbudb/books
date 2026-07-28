9e5ecf288ff5f1797ae946dd4e840165e06292ac
Dennis Budek <dennis.budek@protonmail.com>
2026-07-28 17:35:34 +0200

    create db with Author and Book Table

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 import os
 from data_models import db, Author, Book
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQL_ALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'data/library.sqlite')}"
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    f"sqlite:///{os.path.join(basedir, 'data', 'library.sqlite')}"
+)
 
 db.init_app(app)
 
+with app.app_context():
+    db.create_all()
---
+++ data_models.py
@@ -0,0 +1,41 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Author(db.Model):
+    __tablename__ = "authors"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    birth_date = db.Column(db.Date, nullable=True)  # pretty sure there are Authors whos birthdate is not know
+    date_of_death = db.Column(db.Date, nullable=True)
+
+    books = db.relationship("Book", back_populates="authors")
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return f"ID: {self.id}, Name: {self.name}, Birth: {self.birth_date}, Death: {self.date_of_death}"
+
+
+class Book(db.Model):
+    __tablename__ = "books"
+    id = db.Column(db.Integer, primary_key=True)
+    isbn = db.Column(db.String(20), unique=True, nullable=True)
+    title = db.Column(db.String(200), nullable=False)
+    publication_year = db.Column(db.Integer, nullable=True)
+
+    author_id = db.Column(
+        db.Integer,
+        db.ForeignKey("authors.id"),
+        nullable=False,
+    )
+
+    author = db.relationship("Author", back_populates="books")
+
+    def __str__(self):
+        return self.title
+
+    def __repr__(self):
+        return f"ID: {self.id}, Title: {self.title}, "
