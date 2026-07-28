3767243cc27d25d918a131c006b6878ffe0dcd76
Dennis Budek <dennis.budek@protonmail.com>
2026-07-28 18:48:53 +0200

    first route /add_author added and working

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from data_models import db, Author, Book
-from flask import Flask
+from flask import Flask, render_template, request
+from datetime import date
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -14,3 +15,30 @@ db.init_app(app)
 
 with app.app_context():
     db.create_all()
+
+
+@app.route("/add_author", methods=["GET", "POST"])
+def add_author():
+    if request.method == "GET":
+        return render_template("add_author.html")
+    name = request.form["name"]
+    birth_date = date.fromisoformat(request.form["birthdate"])
+    death_text = request.form["date_of_death"]
+    death_date = date.fromisoformat(death_text) if death_text else None
+    author = Author(
+        name=name,
+        birth_date=birth_date,
+        date_of_death=death_date,
+    )
+
+    db.session.add(author)
+    db.session.commit()
+    return render_template("add_author.html", message="Author added successfully")
+
+
+"""@app.route("/add_book")
+def add_book():
+    return render_template("add_book.html")"""
+
+if __name__ == "__main__":
+    app.run(debug=True)
