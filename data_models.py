e67854309d3fe66ca1ab3c70eb72eea869780c51
Dennis Budek <dennis.budek@protonmail.com>
2026-07-28 17:39:02 +0200

    update gitignore and typos

--- data_models.py
+++ data_models.py
@@ -5,12 +5,13 @@ db = SQLAlchemy()
 
 class Author(db.Model):
     __tablename__ = "authors"
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     birth_date = db.Column(db.Date, nullable=True)  # pretty sure there are Authors whos birthdate is not know
     date_of_death = db.Column(db.Date, nullable=True)
 
-    books = db.relationship("Book", back_populates="authors")
+    books = db.relationship("Book", back_populates="author")
 
     def __str__(self):
         return self.name
@@ -21,16 +22,12 @@ class Author(db.Model):
 
 class Book(db.Model):
     __tablename__ = "books"
+
     id = db.Column(db.Integer, primary_key=True)
     isbn = db.Column(db.String(20), unique=True, nullable=True)
     title = db.Column(db.String(200), nullable=False)
     publication_year = db.Column(db.Integer, nullable=True)
-
-    author_id = db.Column(
-        db.Integer,
-        db.ForeignKey("authors.id"),
-        nullable=False,
-    )
+    author_id = db.Column(db.Integer, db.ForeignKey("authors.id"), nullable=False)
 
     author = db.relationship("Author", back_populates="books")
 
