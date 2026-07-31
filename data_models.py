5ca2d8ba232dbbe21886d0b6e291773edda63a92
Dennis Budek <dennis.budek@protonmail.com>
2026-07-31 13:03:18 +0200

    implements order by in sorting, seeding altered to only add movies with covers to look better

--- data_models.py
+++ data_models.py
@@ -9,7 +9,7 @@ class Author(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    birth_date = db.Column(db.Date, nullable=True)  # pretty sure there are Authors whos birthdate is not know
+    birth_date = db.Column(db.Date, nullable=True)  # pretty sure there are Authors whos birthdate is not known
     date_of_death = db.Column(db.Date, nullable=True)
 
     books = db.relationship("Book", back_populates="author")
